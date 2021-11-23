a45e429cdf5ae90e0f955946e667f787b30e3f0f
Jeffrey Rowe <jeffrey.r.rowe58@gmail.com>
2021-11-23 12:09:00 -0600

    multipage spa

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import {Route} from "react-router-dom";
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+import MainHeader from "./components/MainHeader";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <MainHeader />
+            <main>
+                <Route path="/welcome">
+                    <Welcome/>
+                </Route>
+                <Route path="/products">
+                    <Products/>
+                </Route>
+            </main>
+        </div>
+    );
 }
 
 export default App;
