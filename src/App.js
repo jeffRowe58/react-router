7530d20b27312fd669b146490314d548cb8fe6a9
Jeffrey Rowe <jeffrey.r.rowe58@gmail.com>
2021-11-25 17:50:31 -0600

    dynamic setup started

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {Route} from "react-router-dom";
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Products";
 import MainHeader from "./components/MainHeader";
+import ProductDetails from "./pages/ProductDetails";
 
 function App() {
     return (
@@ -13,6 +14,9 @@ function App() {
                 </Route>
                 <Route path="/products">
                     <Products/>
+                </Route>
+                <Route path='/product-detail/:productId'>
+                    <ProductDetails />
                 </Route>
             </main>
         </div>
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,5 +1,12 @@
 const Products = () => {
-    return <h1>The Products Page</h1>
+    return <section>
+        <h1>The Products Page</h1>
+        <ul>
+            <li>Book</li>
+            <li>Carpet</li>
+            <li>Online Course</li>
+        </ul>
+    </section>
 };
 
 export default Products;
