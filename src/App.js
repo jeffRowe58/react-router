bd29b2821265448911b79b8632e75a456056c5a5
Jeffrey Rowe <jeffrey.r.rowe58@gmail.com>
2021-11-27 09:19:49 -0600

    react router overview complete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Route} from "react-router-dom";
+import {Redirect, Route, Switch} from "react-router-dom";
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Products";
 import MainHeader from "./components/MainHeader";
@@ -7,17 +7,22 @@ import ProductDetails from "./pages/ProductDetails";
 function App() {
     return (
         <div>
-            <MainHeader />
+            <MainHeader/>
             <main>
-                <Route path="/welcome">
-                    <Welcome/>
-                </Route>
-                <Route path="/products">
-                    <Products/>
-                </Route>
-                <Route path='/product-detail/:productId'>
-                    <ProductDetails />
-                </Route>
+                <Switch>
+                    <Route path='/' exact>
+                        <Redirect to='/welcome' />
+                    </Route>
+                    <Route path="/welcome">
+                        <Welcome/>
+                    </Route>
+                    <Route path="/products" exact>
+                        <Products/>
+                    </Route>
+                    <Route path='/products/:productId'>
+                        <ProductDetails/>
+                    </Route>
+                </Switch>
             </main>
         </div>
     );
